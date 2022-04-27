acc14dfb9cfca251b77bed2a90d609967c4ce4cc
ftnext <takuyafjp+develop@gmail.com>
2022-04-28 01:33:37 +0900

    feat(2-4): HitPoint.damage

--- chapter2/first_step.py
+++ chapter2/first_step.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Final
 
 
@@ -29,3 +31,18 @@ class HitPoint:
             raise ValueError(f"{self.MAX}以下を指定してください")
 
         self.value = value
+
+    def damage(self, damage_amount: int) -> HitPoint:
+        """ダメージを受ける
+
+        >>> HitPoint(100).damage(70)
+        HitPoint(value=30)
+        >>> HitPoint(100).damage(108)
+        HitPoint(value=0)
+        """
+        damaged = self.value - damage_amount
+        corrected = self.MIN if damaged < self.MIN else damaged
+        return self.__class__(corrected)
+
+    def __repr__(self) -> str:
+        return f"HitPoint(value={self.value})"
