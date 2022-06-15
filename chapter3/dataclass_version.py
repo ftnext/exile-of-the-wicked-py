8dfb8943c3a0ee0ece696b055d223df4ce946225
ftnext <takuyafjp+develop@gmail.com>
2022-06-15 22:18:14 +0900

    [feat] Freeze Money class

--- chapter3/dataclass_version.py
+++ chapter3/dataclass_version.py
@@ -1,11 +1,21 @@
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(frozen=True)
 class Money:
     """
     >>> Money(100)
     Money(amount=100)
+
+    >>> money = Money(1)
+    >>> money.amount = 10000
+    Traceback (most recent call last):
+        ...
+    dataclasses.FrozenInstanceError: cannot assign to field 'amount'
+    >>> setattr(money, "amount", 10000)
+    Traceback (most recent call last):
+        ...
+    dataclasses.FrozenInstanceError: cannot assign to field 'amount'
     """
 
     amount: int
