a44ee2a1da7b0f0bef06cf8d91a25836f7a02d36
ftnext <takuyafjp+develop@gmail.com>
2022-06-15 22:25:04 +0900

    [feat] Complete constructor with post init

--- chapter3/dataclass_version.py
+++ chapter3/dataclass_version.py
@@ -8,6 +8,11 @@ class Money:
     >>> Money(100)
     Money(amount=100)
 
+    >>> Money(-200)
+    Traceback (most recent call last):
+      ...
+    ValueError: 金額が0以上でありません。
+
     >>> money = Money(1)
     >>> money.amount = 10000
     Traceback (most recent call last):
@@ -20,3 +25,7 @@ class Money:
     """
 
     amount: Final[int]
+
+    def __post_init__(self):
+        if self.amount < 0:
+            raise ValueError("金額が0以上でありません。")
