5ab51bad7adf2816b75720a0c89693fec5c0e744
ftnext <takuyafjp+develop@gmail.com>
2022-07-01 00:30:35 +0900

    [feat] Add Shiden magic

--- chapter6/one_place_fork.py
+++ chapter6/one_place_fork.py
@@ -5,6 +5,7 @@ from enum import Enum, auto
 @dataclass
 class Member:
     level: int
+    agility: int
 
 
 class MagicType(Enum):
@@ -15,9 +16,12 @@ class MagicType(Enum):
 
 class Magic:
     """
-    >>> member = Member(10)
+    >>> level, agility = 10, 20
+    >>> member = Member(level, agility)
     >>> Magic(MagicType.FIRE, member)
     Magic(name='ファイア', cost_magic_point=2, attack_power=25, cost_technical_point=0)
+    >>> Magic(MagicType.SHIDEN, member)
+    Magic(name='紫電', cost_magic_point=7, attack_power=80, cost_technical_point=5)
     """
 
     def __init__(self, magic_type: MagicType, member: Member) -> None:
@@ -27,6 +31,11 @@ class Magic:
                 self.cost_magic_point = 2
                 self.attack_power = 20 + int(member.level * 0.5)
                 self.cost_technical_point = 0
+            case MagicType.SHIDEN:
+                self.name = "紫電"
+                self.cost_magic_point = 5 + int(member.level * 0.2)
+                self.attack_power = 50 + int(member.agility * 1.5)
+                self.cost_technical_point = 5
 
     def __repr__(self) -> str:
         return (
