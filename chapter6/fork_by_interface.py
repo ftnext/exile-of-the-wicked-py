0e65adf5424a2c82375efb5c10178e53e08575aa
ftnext <takuyafjp+develop@gmail.com>
2022-07-01 19:53:23 +0900

    [feat] Initialize Fire Magic

--- chapter6/fork_by_interface.py
+++ chapter6/fork_by_interface.py
@@ -1,4 +1,13 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Member:
+    level: int
+    agility: int
+    magic_attack: int
+    vitality: int
 
 
 class Magic(ABC):
@@ -15,5 +24,28 @@ class Magic(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    def cost_technical_point(self) -> int:
+        raise NotImplementedError
+
+
+class Fire(Magic):
+    """
+    >>> level, agility, magic_attack, vitality = 10, 20, 13, 7
+    >>> member = Member(level, agility, magic_attack, vitality)
+    >>> fire = Fire(member)
+    """
+
+    def __init__(self, member: Member) -> None:
+        self.member = member
+
+    def name(self) -> str:
+        raise NotImplementedError
+
+    def cost_magic_point(self) -> int:
+        raise NotImplementedError
+
+    def attack_power(self) -> int:
+        raise NotImplementedError
+
     def cost_technical_point(self) -> int:
         raise NotImplementedError
