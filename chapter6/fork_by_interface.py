aac517a5445c4a46911f351fd6455b75e45b2ae9
ftnext <takuyafjp+develop@gmail.com>
2022-07-01 19:58:06 +0900

    [feat] Initialize Shiden Magic

--- chapter6/fork_by_interface.py
+++ chapter6/fork_by_interface.py
@@ -57,3 +57,26 @@ class Fire(Magic):
 
     def cost_technical_point(self) -> int:
         return 0
+
+
+class Shiden(Magic):
+    """
+    >>> level, agility, magic_attack, vitality = 10, 20, 13, 7
+    >>> member = Member(level, agility, magic_attack, vitality)
+    >>> shiden = Shiden(member)
+    """
+
+    def __init__(self, member: Member) -> None:
+        self.member = member
+
+    def name(self) -> str:
+        raise NotImplementedError
+
+    def cost_magic_point(self) -> int:
+        raise NotImplementedError
+
+    def attack_power(self) -> int:
+        raise NotImplementedError
+
+    def cost_technical_point(self) -> int:
+        raise NotImplementedError
