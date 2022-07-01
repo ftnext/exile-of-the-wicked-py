46b2d766804d9b8e27ad62677b4ddbeff58dca06
ftnext <takuyafjp+develop@gmail.com>
2022-07-01 19:55:48 +0900

    [feat] Implement Fire

--- chapter6/fork_by_interface.py
+++ chapter6/fork_by_interface.py
@@ -33,19 +33,27 @@ class Fire(Magic):
     >>> level, agility, magic_attack, vitality = 10, 20, 13, 7
     >>> member = Member(level, agility, magic_attack, vitality)
     >>> fire = Fire(member)
+    >>> fire.name()
+    'ファイア'
+    >>> fire.cost_magic_point()
+    2
+    >>> fire.attack_power()
+    25
+    >>> fire.cost_technical_point()
+    0
     """
 
     def __init__(self, member: Member) -> None:
         self.member = member
 
     def name(self) -> str:
-        raise NotImplementedError
+        return "ファイア"
 
     def cost_magic_point(self) -> int:
-        raise NotImplementedError
+        return 2
 
     def attack_power(self) -> int:
-        raise NotImplementedError
+        return 20 + int(self.member.level * 0.5)
 
     def cost_technical_point(self) -> int:
-        raise NotImplementedError
+        return 0
