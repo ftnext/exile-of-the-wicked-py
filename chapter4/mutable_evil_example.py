296c1a752322ef56928c1d471c5ab7cbdf79316b
ftnext <takuyafjp+develop@gmail.com>
2022-08-27 21:34:54 +0900

    [feat] evil using same in multiple threads

--- chapter4/mutable_evil_example.py
+++ chapter4/mutable_evil_example.py
@@ -1,4 +1,6 @@
+import time
 from dataclasses import dataclass
+from threading import Thread
 from typing import ClassVar
 
 
@@ -35,3 +37,50 @@ weapon_a.attack_power.reinforce(5)
 
 assert id(weapon_a.attack_power) == id(weapon_b.attack_power)
 assert weapon_a.attack_power.value == weapon_b.attack_power.value == 25
+
+
+# スレッド間での使い回しの例
+def reinforce_worker(attack_power: AttackPower) -> None:
+    print("reinforce_worker: start")
+    print(f"reinforce_worker: {attack_power.value=}")
+    print("reinforce_worker: REINFORCE!!")
+    attack_power.reinforce(15)
+    print(f"reinforce_worker: {attack_power.value=}")
+    time.sleep(2)
+    print("reinforce_worker: end")
+
+
+def disable_worker(attack_power: AttackPower) -> None:
+    print("disable_worker: start")
+    time.sleep(1)
+    print(f"disable_worker: {attack_power.value=}")
+    print("disable_worker: DISABLE!!")
+    attack_power.disable()
+    print(f"disable_worker: {attack_power.value=}")
+    print("disable_worker: end")
+
+
+attack_power = AttackPower(20)
+
+reinforce_thread = Thread(target=reinforce_worker, args=(attack_power,))
+disable_thread = Thread(target=disable_worker, args=(attack_power,))
+
+reinforce_thread.start()
+disable_thread.start()
+reinforce_thread.join()
+disable_thread.join()
+
+assert attack_power.value == 0
+
+"""
+reinforce_worker: start
+reinforce_worker: attack_power.value=20
+reinforce_worker: REINFORCE!!
+reinforce_worker: attack_power.value=35
+disable_worker: start
+disable_worker: attack_power.value=35
+disable_worker: DISABLE!!
+disable_worker: attack_power.value=0
+disable_worker: end
+reinforce_worker: end
+"""
