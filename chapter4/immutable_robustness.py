c5ff5641fb14ea2de2bd7c0f00f9d0839f8249b6
ftnext <takuyafjp+develop@gmail.com>
2022-09-07 23:33:16 +0900

    [bugfix] Resolve aliasing bug

--- chapter4/immutable_robustness.py
+++ chapter4/immutable_robustness.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import time
 from dataclasses import dataclass
 from threading import Thread
-from typing import ClassVar
+from typing import ClassVar, Final
 
 
 @dataclass(frozen=True)
@@ -31,6 +31,29 @@ class AttackPower:
         return self.__class__(self.MIN)
 
 
+@dataclass(frozen=True)
+class Weapon:
+    attack_power: AttackPower
+
+    def reinforce(self, increment: AttackPower):
+        """武器を強化する"""
+        reinforced: Final[AttackPower] = attack_power.reinforce(increment)
+        return self.__class__(reinforced)
+
+
+# 別名参照問題は解決されている
+attack_power: Final[AttackPower] = AttackPower(20)
+
+weapon_a: Final[Weapon] = Weapon(attack_power)
+weapon_b: Final[Weapon] = Weapon(attack_power)
+
+increment: Final[AttackPower] = AttackPower(5)
+reinforced_weapon_a: Final[Weapon] = weapon_a.reinforce(increment)
+
+assert weapon_a.attack_power.value == weapon_b.attack_power.value == 20
+assert reinforced_weapon_a.attack_power.value == 25
+
+
 # スレッド間で使い回しても問題なし
 def reinforce_worker(attack_power: AttackPower) -> None:
     print("reinforce_worker: start")
@@ -52,14 +75,14 @@ def disable_worker(attack_power: AttackPower) -> None:
     print("disable_worker: end")
 
 
-attack_power = AttackPower(20)
+attack_power2 = AttackPower(20)
 
-reinforce_thread = Thread(target=reinforce_worker, args=(attack_power,))
-disable_thread = Thread(target=disable_worker, args=(attack_power,))
+reinforce_thread = Thread(target=reinforce_worker, args=(attack_power2,))
+disable_thread = Thread(target=disable_worker, args=(attack_power2,))
 
 reinforce_thread.start()
 disable_thread.start()
 reinforce_thread.join()
 disable_thread.join()
 
-assert attack_power.value == 20
+assert attack_power2.value == 20
