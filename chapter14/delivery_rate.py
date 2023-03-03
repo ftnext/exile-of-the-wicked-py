6f77d45f2893b6f11c23138c714faeaab0d6e9a7
ftnext <takuyafjp+develop@gmail.com>
2023-03-03 20:14:27 +0900

    [test] Add tests of delivery charge

--- chapter14/delivery_rate.py
+++ chapter14/delivery_rate.py
@@ -1,6 +1,7 @@
 """14.2 ユニットテストを用いたリファクタリングの例（写経）"""
 from collections.abc import Sequence
 from dataclasses import dataclass
+from unittest import TestCase
 
 
 @dataclass(frozen=True)
@@ -42,3 +43,29 @@ class DeliveryManager:
         else:
             charge = 0
         return charge
+
+
+class DeliveryChargeTest(TestCase):
+    def test_pay_charge(self):
+        """商品の合計金額が2000円未満の場合、配送料は500円。"""
+        empty_cart = ShoppingCart()
+        one_product_added = empty_cart.add(Product(1, "商品A", 500))
+        two_product_added = one_product_added.add(Product(2, "商品B", 1499))
+        charge = DeliveryCharge(two_product_added)
+
+        self.assertEqual(500, charge.amount)
+
+    def test_free_charge(self):
+        """商品の合計金額が2000円以上の場合、配送料は無料。"""
+        empty_cart = ShoppingCart()
+        one_product_added = empty_cart.add(Product(1, "商品A", 500))
+        two_product_added = one_product_added.add(Product(2, "商品B", 1500))
+        charge = DeliveryCharge(two_product_added)
+
+        self.assertEqual(0, charge.amount)
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
